41434848ad7708eaa768bf0c1757eb3db2044ef7
deKoGenius <kdh12045s@naver.com>
2022-05-05 20:30:14 +0900

    커밋용

--- community/models.py
+++ community/models.py
@@ -3,38 +3,38 @@ from django.db import models
 # Create your models here.
 
 
-class Input(models.model): # 처음 입력 모델
+class Input(models.Model): # 처음 입력 모델
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag, blank=True)
     period = models.IntegerField()
     area = models.CharField(max_length=20)
 
-class Tag(models.model): # 태그 모델
+class Tag(models.Model): # 태그 모델
     name = models.CharField(max_length=12)
     value = models.FloatField()
 
-class Tour(models.model): # 관광지 모델
+class Tour(models.Model): # 관광지 모델
     name = models.CharField(max_length=30)
     content_time = models.IntegerField()
     image = models.URLField()
     x_value = models.FloatField()
     y_value = models.FloatField()
 
-class Restaurant(models.model): # 식당 모델
+class Restaurant(models.Model): # 식당 모델
     name = models.CharField(max_length=30)
     content_time = models.IntegerField()
     image = models.URLField()
     x_value = models.FloatField()
     y_value = models.FloatField()
 
-class Stay(models.model): # 숙소 모델
+class Stay(models.Model): # 숙소 모델
     name = models.CharField(max_length=30)
     content_time = models.IntegerField()
     image = models.URLField()
     x_value = models.FloatField()
     y_value = models.FloatField()
 
-class Schedule(models.model): # 일정리스트 모델
+class Schedule(models.Model): # 일정리스트 모델
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag, blank=True)
     tour = models.ManyToManyField(Tour, blank=True)
