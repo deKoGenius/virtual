2bc800c20715925abe2c4307974c808aef681450
ingi-Hwang <hang4736@naver.com>
2022-05-09 15:27:59 +0900

    admin 추가

--- community/admin.py
+++ community/admin.py
@@ -3,5 +3,10 @@ from .models import *
 
 # Register your models here.
 
+admin.site.register(Tag)
+admin.site.register(User)
 admin.site.register(Input)
 admin.site.register(Tour)
+admin.site.register(Restaurant)
+admin.site.register(Stay)
+admin.site.register(Schedule)
