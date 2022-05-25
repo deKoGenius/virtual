763752edbf68e47f57b8490ef4fd56ea235ba7de
deKoGenius <kdh12045s@naver.com>
2022-05-25 11:47:23 +0900

    2022.05.25 working on it

---
+++ community/tag_crawling.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+django.setup()
+from community.models import Tour
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+'''--------------- use selenium ------------------'''
+def get_tour_tag(name):
+    driver = webdriver.Chrome('chromedriver')
+    url = 'https://www.visitbusan.net/index.do?menuCd=DOM_000000201001000000&ucc2_seq=&search_keyword=&list_type=TYPE_SMALL_CARD&order_type=NEW&listCntPerPage2=16'
+    driver.get(url)
+
+    element = WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div/div/div/form/div/div/div[2]/div[1]/input')))
+    element.send_keys(name)
+    element.send_keys(Keys.RETURN)
+    element = WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#containernew > div > div > div > div > div.trvList.food_new > div > div > div.info > p > a'))).click()
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'html.parser')
+    parse = soup.find_all('li')
+    tag = list()
+    for index in parse:
+        tag_name = index.get_text()
+        if "#" in tag_name:
+            tag_name = tag_name[1:]
+            tag.append(tag_name)
+            # if "힐링" in tag_name:
+            #     print(tag_name)
+            # if "산책" in tag_name:
+            #     print(tag_name)
+            # if "테마" in tag_name:
+            #     print(tag_name)
+            # if "박물관" in tag_name:
+            #     print(tag_name)
+            # if "야경" in tag_name:
+            #     print(tag_name)
+            # if "바다" in tag_name:
+            #     print(tag_name)
+    return tag
+
+# driver.switch_to.frame(frame)
+# time.sleep(1)
+#
+# first_content= driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[1]/ul/li/div[2]').click()
+# driver.switch_to.default_content()
+# frame = WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#entryIframe')))
+# driver.switch_to.frame(frame)
+# time.sleep(2)
+#
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#
--- community/tests.py
+++ community/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+django.setup()
+import pandas as pd
+from community.models import Tour
+from community.tag_crawling import get_tour_tag
 
 # Create your tests here.
+
+all = Tour.objects.all()
+print(all[0].__str__())
+tourname = all[0].__str__()
+tag = get_tour_tag(tourname)
+print(tag)
+# for tourname in all:
+#     print(index)
